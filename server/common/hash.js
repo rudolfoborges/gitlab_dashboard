392b78df337237a4761c8b1875cd38a465340b69
Rudolfo Borges <oliveira.rudolfo@gmail.com>
2015-07-03 01:35:46 -0300

    hash refectory

--- server/common/hash.js
+++ server/common/hash.js
@@ -2,40 +2,34 @@
 
 module.exports = function(){
 	var keys = [],
-		values = [];
+			values = [];
 
 	function indexOf(key){
 		return keys.indexOf(key);
 	}
 
-	var clazz = {
-
-		put: function(key, value){
-			var index = indexOf(key);
-			if(index >= 0){
-				values[index] = value;
-			} else{
-				keys.push(key);
-				values.push(value);
-			}
-		},
-
-		get: function(key){
-			var index = indexOf(key);
-			if(index >= 0){
-				return values[index];
-			}
-		},
-
-		contains: function(key){
-			return indexOf(key) >= 0;
-		},
-
-		getKeys: function(){
-			return keys;
+	this.put = function(key, value){
+		var index = indexOf(key);
+		if(index >= 0){
+			values[index] = value;
+		} else{
+			keys.push(key);
+			values.push(value);
 		}
+	}
 
-	};
+	this.get = function(key){
+		var index = indexOf(key);
+		if(index >= 0){
+			return values[index];
+		}
+	}
 
-	return clazz;
+	this.contains = function(key){
+		return indexOf(key) >= 0;
+	}
+
+	this.getKeys = function(){
+		return keys;
+	}
 }
